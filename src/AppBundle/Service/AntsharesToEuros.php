9dc30cdb24d165e57ea8316b9b2068484f5a4205
Vince Liem <vincepaul.liem@gmail.com>
2017-07-03 16:33:17 +0200

    I need to finish the budget subtraction. I need to see the history, per day. Also budget will be seen in payments under budget.

--- src/AppBundle/Service/AntsharesToEuros.php
+++ src/AppBundle/Service/AntsharesToEuros.php
@@ -23,15 +23,21 @@ class AntsharesToEuros
         $this->buzz = $buzz;
     }
 
-    public function getPrice($antshares = 1547)
+    public function getPrice()
     {
-        $result['anc'] = $antshares;
+        $result['ans'] = 1547;
         $result['ANSBTC'] = json_decode($this->buzz->get('https://bittrex.com/api/v1.1/public/getticker?market=btc-ans')->getContent())->result->Last;
 
-        $result['btc'] = $result['anc'] * $result['ANSBTC'];
+        $result['btc'] = $result['ans'] * $result['ANSBTC'];
         $market['bitonic'] = json_decode($this->buzz->get('https://bitonic.nl/api/sell?btc=' . $result['btc'])->getContent());
         $result['eur'] = $market['bitonic']->eur;
         $result['BTCEUR'] = $market['bitonic']->price;
+
+        $market['ANCCNY'] = json_decode($this->buzz->get('https://www.19800.com/api/v1/ticker?market=cny_anc')->getContent())->data->LastPrice;
+        $market['CNYEUR'] = json_decode($this->buzz->get('http://api.fixer.io/latest?base=CNY&symbols=EUR')->getContent())->rates->EUR;
+
+        $result['eur_day'] = 0.7128576 * $market['ANCCNY'] * $market['CNYEUR'];
+        $result['eur_month'] = $result['eur_day'] * 30.5;
 
         return $result;
     }
